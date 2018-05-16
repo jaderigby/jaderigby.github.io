8fb8279651c0120565ae75134da184ccd4500fd7
Jade Rigby <contact@jaderigby.com>
2018-05-16 07:37:30 -0600

    changing up files and adding support for downloading notes; also, created table of contents link file

---
+++ ux-guidelines-vol1/Slides/slides.js
@@ -0,0 +1,159 @@
+const title = "UX Guidelines: Vol. 1"
+
+const slides = [
+{
+"set" : 1,
+"idLabel" : "Principles",
+"type" : "section",
+"content" : `
+# Principles
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Breathing-Room",
+"content" : `
+## Whitespace: Breathing Room
+
+- Whitespace can increase relevance
+  - Points of "focus"
+  - Walks you through the message
+  - More welcoming
+- Examples
+  - [apple](www.apple.com)
+  - [Built by Buffalo](http://builtbybuffalo.com/)
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Dynamic-Content",
+"content" : `
+## Dynamic Content: Animation
+
+- Avoid Gimmicks
+- Let the "device" present the message
+- Should strengthen the <span style="border-bottom: 2px solid">story</span>
+`
+}
+,{
+"set" : 2,
+"idLabel" : "PSD-and-Conventions",
+"type" : "section",
+"content" : `
+<h1>PSDs<br /><span class="split-line">Conventions</span></h1>
+`
+}
+,{
+"set" : 2,
+"idLabel" : "PSD-Standards",
+"content" : `
+## PSD Standards
+
+- Layer Groups:
+  - Logical Order:
+    - Header
+    - Content
+    - Footer
+  - Other groupings, as needed
+- Standard Web Resolution: __72ppi__
+`
+}
+,{
+"set" : 2,
+"idLabel" : "Conventions",
+"content" : `
+## Conventions
+
+- Margins [30px]
+- Gutters [15px]
+- Whitespace
+- Increase left/right padding on buttons [10 - 30px]
+`
+}
+,{
+"set" : 2,
+"idLabel" : "Style-Guide",
+"content" : `
+## Style Guide
+
+- __Online Resource__ (document > online)
+- Standardize Margins and Gutters
+- Buttons, panels, widgets
+- Formalize Fonts
+  - font stacks (arial, verdana, sans-serif)
+- Color Palettes (per brand)
+`
+}
+,{
+"set" : 2,
+"idLabel" : "Mobile-First",
+"content" : `
+## Mobile First?
+
+- Current State of Things
+- Mobile First: A Pragmatic Approach
+  - "Mobile First" not "Mobile is Best"
+- Content = Defensible Position
+`
+}
+,{
+"set" : 2,
+"idLabel" : "Workflow",
+"content" : `
+## Workflow
+
+- Mobile First
+- Ongoing
+- Mobile Motivated Design
+- __Scenario:__
+  - conceptualize content: Mobile First
+  - design: desktop
+  - checkin: mobile first
+  - finish: __fork__ --> a) Desktop, b) Mobile
+
+`
+}
+,{
+"set" : 3,
+"idLabel" : "Tools",
+"type" : "section",
+"content" : `
+# Tools #
+`
+}
+,{
+"set" : 3,
+"idLabel" : "Current-Tools",
+"content" : `
+## Tools ##
+<ul>
+  <li class="fade">Photoshop</li>
+  <li class="fade">Zeplin</li>
+  <li class="fade">Git</li>
+</ul>
+`
+}
+,{
+"set" : 3,
+"idLabel" : "Zeplin",
+"content" : `
+## Zeplin
+
+### Gives you the following: ###
+
+- Font family
+- Font sizes
+- Hex colors
+- Margin and padding between elements
+`
+}
+,{
+"set" : 3,
+"idLabel" : "Zeplin-Demo",
+"deck" : 2,
+"content" : `
+## Zeplin: Demo
+<img class="main" src="img/zeplin-screenshot.png" width="500">
+`
+}
+]
---
+++ ux-guidelines-vol3/Slides/slides.js
@@ -0,0 +1,113 @@
+var slides = [
+{
+"set" : 1,
+"idLabel" : "Versioning",
+"type" : "section",
+"content" : `
+<h1>Versioning<br /><span class="split-line">Guidelines</span></h1>
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Bad-examples",
+"content" : `
+## Have You Ever ... ? ##
+
+<ul>
+  <li class="fade">myFile-new.psd</li>
+  <li class="fade">myFile-new-new.psd</li>
+  <li class="fade">myFile-final.psd</li>
+  <li class="fade">myFile-new-final.psd</li>
+  <li class="fade">myFile-new-new-final.psd</li>
+  <li class="fade">myFile-new-new-final-final.psd</li>
+  <li class="fade">myFile-new-new-final-final-revised.psd</li>
+</ul>
+
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Semantic-Versioning",
+"content" : `
+## Solution: Semantic Versioning ##
+
+<h3 class="center-justify">Example: __1.2.5__</h3>
+<ul>
+  <li class="fade">MAJOR = backwards compatible breaking</li>
+  <li class="fade">MINOR = new feature</li>
+  <li class="fade">PATCH = bug fixes</li>
+</ul>
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Versioning-Design",
+"deck" : 2,
+"content" : `
+## Semantic Versioning: Design ##
+
+<ul>
+  <li class="fade">MAJOR = new concept</li>
+  <li class="fade">MINOR = revised sections of a cohesive original</li>
+  <li class="fade">PATCH = minor tweeks and changes</li>
+</ul>
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Final-Pattern",
+"deck" : 3,
+"content" : `
+## Versioning: Final Result ##
+
+<div class="swap-wrap">
+  <h3 class="swap show center-justify">
+    [nameOfFile]-[version].[extension]
+  </h3>
+  <h3 class="swap center-justify">
+    myFile-1.0.15.psd
+  </h3>
+</div>
+`
+}
+,{
+"set" : 2,
+"idLabel" : "Font-Stacks",
+"type" : "section",
+"content" : `
+# Font Stacks #
+`
+}
+,{
+"set" : 2,
+"idLabel" : "Our-Font-Stacks",
+"content" : `
+## Our Stacks ##
+
+<div class="swap-wrap">
+
+<div class="swap show">
+
+- __montserrat__ = Montserrat, "Helvetica Neue", Helvetica, Arial, sans-serif
+- __raleway__ = Raleway, "Helvetica Neue", Helvetica, Arial, sans-serif
+- __roboto__ = Roboto, Verdana, Helvetica, sans-serif
+- __lato__ = Lato, "Helvetica Neue", Helvetica, Arial, sans-serif
+- __open-sans__ = "Open Sans", Verdana, Helvetica, sans-serif
+
+</div>
+
+<div class="swap">
+
+- __fira-sans__ ...
+- __hind__ ...
+- __pt-sans__ ...
+- __pt-sans-narrow__ ...
+- __helvetica-neue__ ...
+- __helvetica__ ...
+- __play__ ...
+
+</div>
+</div>
+`
+}
+]
