e1da1f01b859353ea3fe5ec1d5b99c521acaeed6
Jade Rigby <contact@jaderigby.com>
2018-05-19 08:42:01 -0600

    adding to slides

--- ux-guidelines-vol3/Slides/slides.js
+++ ux-guidelines-vol3/Slides/slides.js
@@ -15,12 +15,25 @@ const slides = [
 "set" : 1,
 "idLabel" : "Metadata-Intro",
 "content" : `
-## Metadata Introduction ##
+## Metadata: What Is It? ##
+
+<div class="swap-wrap">
+
+  <div class="swap">
+
+> Often times, we rely on file names and folder structure to organize our files, and consider metadata "extra work".  But in reality, metadata can be even more valuable in maintaining and retrieving files than the folder structure that we contain them in.
+
+  </div>
+  <div class="swap">
 
 - Most files contain metadata
 - Metadata varies from file type to file type
 - PSDs contain metadata
 - Word docs do, as well
+
+  </div>
+
+</div>
 `
 }
 ,{
@@ -34,6 +47,7 @@ const slides = [
     - Creation Date
     - Modified Date
     - Author
+    - Subject
     - Keywords
     - Comments
 `
@@ -42,7 +56,7 @@ const slides = [
 "set" : 1,
 "idLabel" : "Metadata-Where",
 "content" : `
-## Where to Edit Metadata ##
+## Metadata: Where? ##
 
 - Metadata is usually listed under "File > Properties" on Macs.
 - In Photoshop, it is found under "File > File Info"
@@ -50,11 +64,11 @@ const slides = [
 }
 ,{
 "set" : 1,
-"idLabel" : "Metadata-What",
+"idLabel" : "Metadata-Our-Main-Focus",
 "content" : `
-## What Metadata Do We Want? ##
+## Metadata: Our Main Focus ##
 
-- __4__ Most Important Metadata Elements:
+- __4__ Most Important Metadata Elements To Us:
   1. Creation Date
   2. Modified Date
   3. Author
@@ -68,12 +82,102 @@ const slides = [
 "content" : `
 ## Why These? ##
 
-- __Creation Date__ &mdash; Gives us a view into the timeline of creation
+- __Creation Date__ &mdash; Gives us the first view into the creation timeline
 - __Modified Date__ &mdash; Gives a sense of relevancy
-- __Author__ &mdash; Who has ownership of the creative (who to ask questions)
-- __Keywords__ &mdash; For intelligent searches and linking different assets to the same project (will be covered coming up)
-  - __Example__ SeroVital Neck Cream
-    - Keywords SeroVital, Neck
+- __Author__ &mdash; Ownership: Who to ask questions
+- __* Keywords__ &mdash; For intelligent searches and linking different assets to the same project (will be covered coming up)
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Metadata-Kewords",
+"content" : `
+## Metadata: Keywords ##
+
+<div class="swap-wrap">
+
+  <div class="swap">
+
+- Kewords serve as a robust indexing feature
+- Like categories, only better!
+- Keep specific and succinct, but use as many as seem logical
+
+  </div>
+
+  <div class="swap">
+
+### Example: ###
+
+- SeroVital
+- Neck Cream
+- New Years
+- Sale
+- Landing Page
+
+  </div>
+
+  <div class="swap">
+
+Don't worry about classifications such as file type, etc:  Start from general and go to specific, leaving out the generic, such as "file type" &mdash; These are already part of the file's definition.
+
+  </div>
+
+  <div class="swap">
+
+1. __Product__ &mdash; SeroVital, GF-9, etc
+2. __Category__ &mdash; Neck Cream, etc
+3. __Campaign__ &mdash; Memorial Day, etc
+4. __Classification__ &mdash; Landing Page, Email, etc
+
+__Note:__ Reserve Multiple authors for comments section.
+
+  </div>
+
+  <div class="swap">
+
+We can continue to refine our conventions, as we see fit.
+
+__Our main goal is retrieving assets without any wasted time whatsoever: To be able to get what we want, and go to work!__
+
+  </div>
+
+</div>
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Metadata-Then-What",
+"deck" : 2,
+"content" : `
+## Metadata: Then What? ##
+
+<div class="swap-wrap">
+
+  <div class="swap">
+
+### How To Search: ###
+
+- __Keyword Combinations__
+  - "this AND that"
+  - "SeroVital AND Memorial Day AND Sale AND Email"
+- __File type__
+  - "kind:word"
+
+  </div>
+
+  <div class="swap">
+
+### All Together: ###
+
+"SeroVital AND Memorial Day AND Sale AND Email kind:word"
+
+  </div>
+
+</div>
+
+
+
+
 `
 }
 ]
