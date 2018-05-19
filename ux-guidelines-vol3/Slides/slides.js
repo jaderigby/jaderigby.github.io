a21221315d2f2e30f37b7fa43710ca6ecceed799
Jade Rigby <contact@jaderigby.com>
2018-05-18 23:22:09 -0600

    dialing in styles

--- ux-guidelines-vol3/Slides/slides.js
+++ ux-guidelines-vol3/Slides/slides.js
@@ -5,112 +5,66 @@ const date = "5/22/2018";
 const slides = [
 {
 "set" : 1,
-"idLabel" : "Versioning",
+"idLabel" : "Metadata",
 "type" : "section",
-"content" : `<h1>Versioning<br /><span class="split-line">Guidelines</span></h1>
-`
-}
-,{
-"set" : 1,
-"idLabel" : "Bad-examples",
 "content" : `
-## Have You Ever ... ? ##
-
-<ul>
-  <li class="fade">myFile-new.psd</li>
-  <li class="fade">myFile-new-new.psd</li>
-  <li class="fade">myFile-final.psd</li>
-  <li class="fade">myFile-new-final.psd</li>
-  <li class="fade">myFile-new-new-final.psd</li>
-  <li class="fade">myFile-new-new-final-final.psd</li>
-  <li class="fade">myFile-new-new-final-final-revised.psd</li>
-</ul>
-
+# Metadata #
 `
 }
 ,{
 "set" : 1,
-"idLabel" : "Semantic-Versioning",
+"idLabel" : "Metadata-Intro",
 "content" : `
-## Solution: Semantic Versioning ##
+## Metadata Introduction ##
 
-<h3 class="center-justify">Example: __1.2.5__</h3>
-<ul>
-  <li class="fade">MAJOR = backwards compatible breaking</li>
-  <li class="fade">MINOR = new feature</li>
-  <li class="fade">PATCH = bug fixes</li>
-</ul>
+- Most files contain metadata
+- Metadata varies from file type to file type
+- PSDs contain metadata
+- Word docs do, as well
+- Though metadata varies from file type to file type, they typically include the following:
+    - Creation Date
+    - Modified Date
+    - Author
+    - Keywords
+    - Comments
 `
 }
 ,{
 "set" : 1,
-"idLabel" : "Versioning-Design",
+"idLabel" : "Metadata-Where",
+"content" : `
+## Where to Edit Metadata ##
+
+- Metadata is usually listed under "File > Properties" on Macs.
+- In Photoshop, it is found under "File > File Info"
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Metadata-What",
+"content" : `
+## What Metadata Do We Want? ##
+
+- __4__ Most Important Metadata Elements:
+  1. Creation Date
+  2. Modified Date
+  3. Author
+  4. Keywords
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Metadata-Why",
 "deck" : 2,
 "content" : `
-## Semantic Versioning: Design ##
+## Why These? ##
 
-<ul>
-  <li class="fade">MAJOR = new concept</li>
-  <li class="fade">MINOR = revised sections of a cohesive original</li>
-  <li class="fade">PATCH = minor tweeks and changes</li>
-</ul>
-`
-}
-,{
-"set" : 1,
-"idLabel" : "Final-Pattern",
-"deck" : 3,
-"content" : `
-## Versioning: Final Result ##
-
-<div class="swap-wrap">
-  <h3 class="swap show center-justify">
-    [nameOfFile]-[version].[extension]
-  </h3>
-  <h3 class="swap center-justify">
-    myFile-1.0.15.psd
-  </h3>
-</div>
-`
-}
-,{
-"set" : 2,
-"idLabel" : "Font-Stacks",
-"type" : "section",
-"content" : `
-# Font Stacks #
-`
-}
-,{
-"set" : 2,
-"idLabel" : "Our-Font-Stacks",
-"content" : `
-## Our Stacks ##
-
-<div class="swap-wrap">
-
-<div class="swap show">
-
-- __montserrat__ = Montserrat, "Helvetica Neue", Helvetica, Arial, sans-serif
-- __raleway__ = Raleway, "Helvetica Neue", Helvetica, Arial, sans-serif
-- __roboto__ = Roboto, Verdana, Helvetica, sans-serif
-- __lato__ = Lato, "Helvetica Neue", Helvetica, Arial, sans-serif
-- __open-sans__ = "Open Sans", Verdana, Helvetica, sans-serif
-
-</div>
-
-<div class="swap">
-
-- __fira-sans__ ...
-- __hind__ ...
-- __pt-sans__ ...
-- __pt-sans-narrow__ ...
-- __helvetica-neue__ ...
-- __helvetica__ ...
-- __play__ ...
-
-</div>
-</div>
+- __Creation Date__ &mdash; Gives us a view into the timeline of creation
+- __Modified Date__ &mdash; Gives a sense of relevancy
+- __Author__ &mdash; Who has ownership of the creative (who to ask questions)
+- __Keywords__ &mdash; For intelligent searches and linking different assets to the same project (will be covered coming up)
+  - __Example__ SeroVital Neck Cream
+    - Keywords SeroVital, Neck
 `
 }
 ]
