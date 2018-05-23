c4b6f4f34751a4b5b3fe5fb08371713cf94171e9
Jade Rigby <contact@jaderigby.com>
2018-05-23 17:57:39 -0600

    adding scount EDGE night

---
+++ EDGE/Slides/slides.js
@@ -0,0 +1,23 @@
+const title = "Teaching Night";
+
+const date = "5/23/2018";
+
+const slides = [
+{
+"set" : 1,
+"idLabel" : "First-Aid",
+"type" : "section",
+"content" : `
+# First Aid #
+`
+}
+,{
+"set" : 1,
+"idLabel" : "4A",
+"content" : `
+## Section 4A ##
+
+
+`
+}
+]
