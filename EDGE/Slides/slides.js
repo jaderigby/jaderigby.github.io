c1baab7fb969e218104e2a61eb189ddb67af8f6b
Jade Rigby <contact@jaderigby.com>
2018-05-23 18:20:21 -0600

    slide changes

--- EDGE/Slides/slides.js
+++ EDGE/Slides/slides.js
@@ -17,6 +17,62 @@ const slides = [
 "content" : `
 ## Section 4A ##
 
+#### Show first aid for the following: ####
+
+<div class="swap-wrap">
+
+  <div class="swap show">
+
+  - Simple cuts and scrapes
+    - Wash your hands
+    - Stop the bleeding
+    - Clean the wound
+    - Apply antibiotic or petroleum jelly
+    - Cover the wound
+    - Change the dressing
+    - Tetnis shot, if needed (deep or dirty wound; if past five years)
+    - Watch for signs of infection
+
+  </div>
+
+  <div class="swap">
+
+  - Blisters on hand and foot
+    - Wash your hands and the blister
+    - Swab the blister with iodine
+    - Sterilize a clean, sharp needle (rubbing alcohol)
+    - Puncture the blister
+    - Apply ointment (petroleum jelly)
+    - check it each day
+      - cut away dead skin after several days
+
+  </div>
+
+  <div class="swap">
+
+  - Minor burns or scalds (superficial or first-degree)
+    - Cool the burn
+    - Remove rings or other tight items from the burned area
+    - Don't break blisters
+    - Apply lotion
+    - Bandage the burn
+    - if needed, take an over-the-counter pain reliever
+
+  </div>
+
+  <div class="swap">
+
+  - Minor burns or scalds (superficial or first-degree)
+    - Cool the burn
+    - Remove rings or other tight items from the burned area
+    - Don't break blisters
+    - Apply lotion
+    - Bandage the burn
+    - if needed, take an over-the-counter pain reliever
+
+  </div>
+
+</div>
 
 `
 }
