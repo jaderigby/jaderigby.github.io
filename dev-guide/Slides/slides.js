09cc2e31463127f11c4972f2eacc04c37bb5a016
Jade Rigby <contact@jaderigby.com>
2018-06-22 09:40:03 -0600

    adding dev guide to repo

---
+++ dev-guide/Slides/slides.js
@@ -0,0 +1,21 @@
+const title = "Dev Guide";
+
+const date = "6/22/2018";
+
+const slides = [
+{
+"set" : 1,
+"idLabel" : "Dev-Guide",
+"type" : "section",
+"content" : `
+# Dev Guide #
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Metadata-Intro",
+"content" : `
+
+`
+}
+]
