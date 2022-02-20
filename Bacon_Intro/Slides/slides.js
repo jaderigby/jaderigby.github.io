825f0e7270bb6fec76e0f1b05290b5435ccf9f83
Jade Rigby <jaderigby@PL-Jade-Rigby.local>
2022-02-19 18:29:17 -0700

    adding slide content

---
+++ Bacon_Intro/Slides/slides.js
@@ -0,0 +1,41 @@
+const title = "Bacon Walkthrough";
+
+const date = "2/19/2022";
+
+const slides = [
+{
+"set" : 1,
+"idLabel" : "Installation",
+"type" : "section",
+"content" : `
+# Installation #
+`
+}
+,{
+"set" : 1,
+"idLabel" : "Documents-Folder",
+"content" : `
+## Install ##
+
+<div class="fade-sequence">
+<div class="documents-folder">
+
+>>![](images/file-folder-filled-blue.svg)<<
+
+>>__Documents__<<
+
+</div>
+<div class="zip-file">
+
+![](images/file-blue.svg)
+
+</div>
+<div class="repo">
+
+![](images/file-blue.svg)
+
+</div>
+</div>
+`
+}
+]
